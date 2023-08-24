6b48adc7e358310a0dc08f907d28d97bb80e9dfa
allan <allan.konar+github@gitguardian.com>
2023-08-24 16:08:15 -0400

    testing pre-commit hooks

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
@@ -6,6 +6,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
 
-  public static final String password = "gr2Y$mtijY3#wo&dRZ!Y%A&6Re2%gcdBkB3tk5U3XNE";
+  public static final String password = "gr2Y$mtijY3#wo&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE";
   public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
 }
