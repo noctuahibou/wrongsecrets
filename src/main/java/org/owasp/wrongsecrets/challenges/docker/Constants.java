ec75d79327604ecc2e607d8bd01cc92b3c15436a
allan <allan.konar+github@gitguardian.com>
2023-10-10 13:43:50 -0400

    testing honeytokens

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
 
   public static final String password = "gr2issldgY$mMiidjY3#wo78&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE";
   public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
+  public static final String aws_access_key_id = "AKIAWY56YWJJXR2TRJMS"
+  public static final String aws_secret_access_key = "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"
 }
