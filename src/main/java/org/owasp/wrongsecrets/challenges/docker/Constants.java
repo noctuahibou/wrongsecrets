0a2c23abe3959e2778805eeb9340292f3b73d536
allan <allan.konar+github@gitguardian.com>
2023-10-18 13:50:06 -0400

    update

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
@@ -8,6 +8,4 @@ public class Constants {
   #change
   public static final String password = "gr2issldgY$mMiidjY3#wo78&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE";
   public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
-  public static final String aws_access_key_id = "AKIAWY56YWJJZ655YRFB";
-  public static final String aws_secret_access_key = "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";
 }
