84df7324ff68ffd69dc05669eb0833667834c4ba
allan <allan.konar+github@gitguardian.com>
2024-09-03 14:51:04 -0400

    Testing webhooks

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
@@ -1,14 +1,13 @@
 package org.owasp.wrongsecrets.challenges.docker;
 
 import lombok.experimental.UtilityClass;
-
 /** used for Challenges their secrets. */
 @UtilityClass
 
 public static final String aws_access_key_id = "AKIAVAGUQ5JEQBM26JM"
 public static final String aws_secret_access_key = "BkjyUWPzJan5o2+P7fSj78yLkDiKwn/zz5Mfl4v"
 
-# This is a test
+# This is not a test
 public class Constants {
   #change
   public static final String password = "groo2oasgrrgmkhfdgldgY$mMiidjY3#wo78&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE";
