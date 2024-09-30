d63a870c9e3fd7f08445cf3bbe42ab5efc101325
allan <allan.konar+github@gitguardian.com>
2024-09-30 13:55:07 -0400

    Skipping pre-commit hook scan

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
@@ -1,8 +1,6 @@
 Note-2: Did you know that these are working access keysfootnote:disclaimer[They are not "normal" AWS access keys: they are canary tokens! Though you can do `aws sts get-caller-identity` with them. When you use them, some of your data (IP/agent) is being logged.]?!
 Go to https://wrongsecrets.herokuapp.com/stats[stats] when you tried them to find out more!
 
-# THis is a comment
-
 [default]
 aws_access_key_id=AKIASP2TPHJS6R72AFU2
 aws_secret_access_key=tpRsdgLTDr0/asrgadrhPTZtUkS1rCUeWzQvknekDIpe4U3cxbv
@@ -12,7 +10,7 @@ output=json
 aws_access_key_id=AKIASP2TPHJS6R72AFU2aws_secret_access_key=tpRLTDr0/PTZtUkS1rCUeWzQvknekDIpe4U3cxbv
 [default]
 aws_access_key_id=AKIASP2TPHJS4XUU3EPJ
-aws_secret_access_key=CU0ksakjgliwnlajgkloKt4Gt1lHDtJjnRLfdBUZWadmYIHevq/TyUz/
+aws_secret_access_key=CU0lkahgkljklhaljlsafsdkjliwnlajgkloKt4Gt1lHDtJjnRLfdBUZWadmYIHevq/TyUz/
 region=us-east-2
 output=json
 #https://canarytokens.org/manage?token=n0cnd92mavmv1m61tjmyj9of5&auth=6519be82ef910868529091527c3edb3f
