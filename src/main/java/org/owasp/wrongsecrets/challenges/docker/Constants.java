5280662accf3ce74bec110e820c7e6a07e345028
allan <allan.konar+github@gitguardian.com>
2024-11-14 11:34:57 -0500

    skip pre-commit

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
@@ -8,6 +8,6 @@ public static final String aws_access_key_id = "AKIAVAGUQ5JEQBM26JM"
 public static final String aws_secret_access_key = "BkjyUWPzJan5o2+P7fSj78yLkDiKwn/zz5Mfl4v"
 
 public class Constants {
-  public static final String password = "groo2oasgsadgjkljrsagdfghrgmkhfdgldgY$mMiidjY3#wo78&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE"
+  public static final String password = "grosgsadgjkljrsagdfghrgmkhfdgldgY$mMiidjY3#wo78&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE"
   public static final String newKey = "mIgkfsahklysahgebgdD0En55Fq8FXbUfX720K8Vc6/aQYtkFshglmkp7ntsM="
 }
