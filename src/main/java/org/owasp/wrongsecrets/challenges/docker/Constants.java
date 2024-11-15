fbf1da4663830b504241423683e66fcba3b27472
allan <allan.konar+github@gitguardian.com>
2024-11-15 10:38:54 -0500

    skip pre-commit

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/Constants.java
@@ -7,6 +7,7 @@ import lombok.experimental.UtilityClass;
 public static final String aws_access_key_id = "AKIAVAGUQ5JEQBM26JM"
 public static final String aws_secret_access_key = "BkjyUWPzJan5o2+P7fSj78yLkDiKwn/zz5Mfl4v"
 
+# This is a comment
 public class Constants {
   public static final String password = "grosgsadgjkljrsagdfghrgmkhfdgldgY$mMiidjY3#wo78&dRZ!Y%gA&6Re2%gcdBkB3tk5U3XNE"
   public static final String newKey = "mIgkfsaklahgkljklhklysahgebgdD0En55Fq8FXbUfX720K8Vc6/aQYtkFshglmkp7ntsM="
