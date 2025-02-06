954238302b74a263ea34b088a157b9f359e85196
noctuahibou <141830616+noctuahibou@users.noreply.github.com>
2025-02-06 08:11:16 -0500

    includes dev/test secret with no privs

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
@@ -1,4 +1,7 @@
 # Nothing to see here
 aws_access_key_id=AKIAY6HM2ZTJL27IJAFG
-aws_secret_access_key=okjkkasjgfkljhljdfhdhakljasklgjlkglkjlcaikasjgklsdjgkis7O8skljkljlkjklxzfz+y35FGry9+hqvtUuFGb2aD
+aws_secret_access_key=okkljasjdddgfkljhljdfhdfsadghakljasklgjlkglkjlcaikasjgklsdjgkis7O8skljkljlkjklxzfz+y35FGry9+hqvtUuFGb2aD
+# Honeytoken
+aws_access_key_id = AKIAY6HM2ZTJIALUZ3XX
+aws_secret_access_key = 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
 #aws_secret_access_key=ocasK+uf7OPF8sxzfz+y35FGry9+hqvtUuFGb2aD
