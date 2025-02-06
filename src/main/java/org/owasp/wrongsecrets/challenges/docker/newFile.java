df9e1ed910c2500eda12d647412199aac99f0dfe
noctuahibou <141830616+noctuahibou@users.noreply.github.com>
2025-02-06 15:34:24 -0500

    I use this for dv/test

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
@@ -1,6 +1,6 @@
 # Nothing to see here
 aws_access_key_id=AKIAY6HM2ZTJL27IJAFG
-aws_secret_access_key=okkljasjdddgfkljhljdfhdfsadghakljasklgjlkglkjlcaikasjgklsdjgkis7O8skljkljlkjklxzfz+y35FGry9+hqvtUuFGb2aD
+aws_secret_access_key=okkljjkkkasjdkkkddgfkljhljdfhdfsadghakljasklgjlkglkjlcaikasjgklsdjgkis7O8skljkljlkjklxzfz+y35FGry9+hqvtUuFGb2aD
 # Honeytoken
 aws_access_key_id = AKIAY6HM2ZTJIALUZ3XX
 aws_secret_access_key = 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
