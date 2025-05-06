bd25721d7a498803f1c599e17f10c394433a467a
noctuahibou <141830616+noctuahibou@users.noreply.github.com>
2025-05-06 07:17:15 -0400

    testing multiple occurrences

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
@@ -1,8 +1,16 @@
-# Nothing to see here
-aws_access_key_id = AKIAVGOXYQ4SZV4V55ZK
-aws_secret_access_key = 2EpaBCkjkjllV9LCG6ZQ0778Bun9+fG7AZ+j7+fwU6zW3I
+# Nothing to see hereokjkkasjkshgldhlgjlkglkjlcaikasjgklsdjgkis7O8skljkljlkjklxzfz+y35FGry9+hqvtUuFGb2aD
+aws_access_key_id=AKIAY6HM2ZTJL27IJAFG
+aws_secret_access_key=
 
 # Honeytoken
 aws_access_key_id = AKIAY6HM2ZTJIALUZ3XX
-aws_secret_access_key = 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
-#aws_secret_access_key=ocasK+uf7OPF8sxzfz+y35FGry9+hqvtUuFGb2aD
+aws_secret_access_key = SxCGkhjhhddjfyYgYQ2KDYj2NLogHEK+wVbvH34dJHa7XBUd
+
+# This is another test
+    */
+  final protected def storeGlobalMetaData[A: JsonSchema](key: String, value: A): Future[Unit] =
+    graph.persistor.setMetaData(key, Some(encodeMetaData(value)))
+    aws_access_key_id = AKIAZ7TBUSG3PSPVQ3VO
+    aws_secret_access_key = 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
+  final protected def deleteGlobalMetaData(key: String): Future[Unit] =
+    graph.persistor.setMetaData(key, None)
