705865e746cebcd05844fd921194a48cac1ef9df
noctuahibou <141830616+noctuahibou@users.noreply.github.com>
2025-05-13 13:27:36 -0400

    Skipping pre-commit

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/newFile.java
@@ -4,7 +4,7 @@ aws_secret_access_key=
 
 # Honeytoken
 aws_access_key_id = AKIAY6HM2ZTJIALUZ3XX
-aws_secret_access_key = SxCGkhjhhddjfyYgYQ2KDYj2NLogHEK+wVbvH34dJHa7XBUd
+aws_secret_access_key = SxChksdgjkhGkhjhhddjfyYgYQ2KDYj2NLogHEK+wVbvH34dJHa7XBUd
 
     */
   final protected def storeGlobalMetaData[A: JsonSchema](key: String, value: A): Future[Unit] =
