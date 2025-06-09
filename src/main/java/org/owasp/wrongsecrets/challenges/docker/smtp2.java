6ae26e3d3ed78ddc2e1a4a139ecbe0fcd5999884
noctuahibou <141830616+noctuahibou@users.noreply.github.com>
2025-06-09 12:01:50 -0400

    skipping pre-commit

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/smtp2.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/smtp2.java
@@ -35,7 +35,7 @@ public class EmailUtil {
 
 		/** SMTP creds **/
 		<system-property smtp.username="errorReporter"/>
-		<system-property smtp.password="jakhb09ywtbqug9OIWHGOWIHOIHVB3QKQEohoih0"/>
+		<system-property smtp.password="HGOWahrIHOI23yheH54j4wVB3QKQE5w4j45ohoih0"/>
 
 		
 		try
