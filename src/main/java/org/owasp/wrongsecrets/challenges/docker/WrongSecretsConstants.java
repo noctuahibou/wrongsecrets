9fb3041712fd1827b43b823913b9f4d7421a3352
noctuahibou <Glaucus@noctuahibou.com>
2025-08-04 11:57:03 -0400

    skipping pre-commit

--- src/main/java/org/owasp/wrongsecrets/challenges/docker/WrongSecretsConstants.java
+++ src/main/java/org/owasp/wrongsecrets/challenges/docker/WrongSecretsConstants.java
@@ -8,4 +8,6 @@ public class WrongSecretsConstants {
 
   public static final String password = "DefaultLoginPasswordDoNotChange!";
   public static final String newKey = "mISydkkfD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
+
+  mongosh "mongodb+srv://cluster0.0qe9ipy.mongodb.net/" --apiVersion 1 --username allankonar --password QDFH4HJ0zBuJlPLX
 }
